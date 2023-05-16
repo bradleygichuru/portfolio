60ec789a230cc0873bcf7b60df8228547f0739f4
bradleygichuru <prz03@protonmail.com>
2023-05-17 02:54:19 +0300

    build:removes linting before build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://bradleygichuru.github.io",
-  base: "/",
+  base: "/portfolio",
 });
